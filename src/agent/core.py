ab1f7d2fe90020a60a48fe03236706c105e152ce
djalv <alvaroneto98@protonmail.com>
2025-10-12 18:05:45 -0300

    feat: Implement the main agent loop in core.py

---
+++ src/agent/core.py
@@ -0,0 +1,31 @@
+import time
+import socket
+import config
+import state
+import comms
+
+
+def run_agent_loop():
+    api_url, sleep_interval = config.load_config()
+
+    while True:
+        agent_id = state.get_agent_id()
+        hostname = socket.gethostname()
+
+        payload = {"hostname": hostname}
+
+        if agent_id:
+            payload["agentId"] = agent_id
+
+        response_checkin = comms.perform_checkin(api_url, payload)
+
+        if response_checkin:
+            if "agentId" in response_checkin:
+                new_agent_id = response_checkin["agentId"]
+                state.save_agent_id(new_agent_id)
+
+            # -- Response task processing --
+        else:
+            print(f"Check-in failed. Trying again in {sleep_interval} seconds.")
+
+        time.sleep(sleep_interval)
