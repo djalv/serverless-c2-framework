37e17d69cae2274dee93256a47aa256126c93cac
djalv <alvaroneto98@protonmail.com>
2025-10-13 23:47:38 -0300

    feat: Structure of command line commands implemented

---
+++ src/operator_cli/operator.py
@@ -0,0 +1,28 @@
+import click
+import aws_commands
+import formatter
+
+
+@click.group()
+def cli():
+    # --- Command line interface for the C2 Serverless Framework ---
+    pass
+
+
+@cli.command()
+def agents():
+    print("Searching for agents...")
+    agents_list = aws_commands.list_agents()
+    formatter.print_agents_table(agents_list)
+
+
+"""
+@cli.command()
+@click.argument("agent_id")
+@click.argument("command")
+def task(agent_id, command_string):
+    aws.commands.send_task_to_agent(agent_id, command)
+"""
+
+if __name__ == "__main__":
+    cli()
