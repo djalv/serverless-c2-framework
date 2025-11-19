34dee51a0877208d203d4c24e514aa3b435b949b
djalv <alvaroneto98@protonmail.com>
2025-11-18 23:29:34 -0300

    test: Added some unit tests to the agent module

--- src/agent/config.py
+++ src/agent/config.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 
 def load_config():
@@ -8,11 +9,24 @@ def load_config():
 
     config = configparser.ConfigParser()
     config_file_path = "src/agent/config.ini"
+
     if not config.read(config_file_path):
         raise FileNotFoundError(f"File not found: {config_file_path}")
 
-    api_url = config["c2"]["api_url"]
-    sleep_interval = config.getint("c2", "sleep_interval")
-    results_url = config["c2"]["results_url"]
+    try:
+        c2_section = config["c2"]
 
-    return api_url, sleep_interval, results_url
+        api_url = c2_section["api_url"]
+        sleep_interval = config.getint("c2", "sleep_interval")
+        results_url = c2_section["results_url"]
+
+        return api_url, sleep_interval, results_url
+
+    except KeyError as e:
+        print(f"[CRITICAL] Incomplete configuration. Missing key: {e}")
+        raise
+
+    except ValueError as e:
+        print(f"[CRITICAL] Invalid value in configuration: {e}")
+        raise
+
--- tests/unit/test_agent.py
+++ tests/unit/test_agent.py
@@ -51,6 +51,16 @@ def test_perform_checkin_returns_none_on_network_failure(mock_post, api_setup):
         api_setup["url"], json=api_setup["data"], timeout=10
     )
 
+@patch("src.agent.comms.requests.post")
+def test_perform_checkin_returns_none_on_http_error(mock_post, api_setup):
+    mock_post.side_effect = requests.exceptions.HTTPError()
+
+    result = perform_checkin(api_setup["url"], api_setup["data"])
+
+    assert result is None
+    mock_post.assert_called_once_with(
+        api_setup["url"], json=api_setup["data"], timeout=10
+    )
 
 @patch("src.agent.comms.requests.post")
 def test_send_results_returns_true_on_http_200(mock_post, api_setup):
@@ -73,6 +83,21 @@ def test_send_results_returns_true_on_http_200(mock_post, api_setup):
 @patch("src.agent.comms.requests.post")
 def test_send_results_returns_false_on_network_failure(mock_post, api_setup):
     mock_post.side_effect = requests.exceptions.Timeout()
+
+    agent_id = api_setup["data"]["agentId"]
+    task_result = api_setup["task_result"]
+    payload = {"agentId": agent_id, "taskResult": task_result}
+
+    result = send_results(api_setup["results_url"], agent_id, task_result)
+
+    assert not result
+    mock_post.assert_called_once_with(
+        api_setup["results_url"], json=payload, timeout=10
+    )
+
+@patch("src.agent.comms.requests.post")
+def test_send_results_returns_false_on_http_error(mock_post, api_setup):
+    mock_post.side_effect = requests.exceptions.HTTPError()
 
     agent_id = api_setup["data"]["agentId"]
     task_result = api_setup["task_result"]
@@ -136,8 +161,49 @@ def test_load_config_raises_error_when_file_not_found(mock_config_parser, config
     mock_instance.read.assert_called_once_with("src/agent/config.ini")
 
 
+@patch("configparser.ConfigParser")
+def test_load_config_raises_error_when_missing_key(mock_config_parser, config_setup):
+    mock_instance = MagicMock()
+
+    mock_section = {
+        "sleep_interval": str(config_setup["sleep_interval"]),
+        "results_url": config_setup["results_url"],
+    }
+
+    mock_instance.__getitem__.return_value = mock_section
+    mock_instance.getint.return_value = config_setup["sleep_interval"]
+
+    mock_config_parser.return_value = mock_instance
+
+    with pytest.raises(KeyError):
+        load_config()
+    
+    mock_config_parser.assert_called_once()
+    mock_instance.read.assert_called_once_with("src/agent/config.ini")
+
+
+@patch("configparser.ConfigParser")
+def test_load_config_raises_error_when_invalid_value(mock_config_parser, config_setup):
+    mock_instance = MagicMock()
+
+    mock_section = {
+        "api_url": config_setup["api_url"],
+        "results_url": config_setup["results_url"],
+    }
+
+    mock_instance.__getitem__.return_value = mock_section
+    mock_instance.getint.side_effect = ValueError("invalid literal for int() with base 10")
+    mock_config_parser.return_value = mock_instance
+
+    with pytest.raises(ValueError):
+        load_config()
+    
+    mock_config_parser.assert_called_once()
+    mock_instance.read.assert_called_once_with("src/agent/config.ini")
+
+
 # Testing agent.state
-def test_get_agent_id_returns_agent_id(mocker):
+def test_get_agent_id_reads_id_from_existing_file(mocker):
     fake_file_content = "  EXISTING-AGENT-ID-456  \n"
     expected_id = "EXISTING-AGENT-ID-456"
 
@@ -149,12 +215,21 @@ def test_get_agent_id_returns_agent_id(mocker):
     assert agent_id == expected_id
 
 
-def test_get_agent_id_returns_none(mocker):
+def test_get_agent_id_returns_none_when_file_not_found(mocker):
     mocker.patch("os.path.exists", return_value=False)
 
     agent_id = get_agent_id()
     assert agent_id is None
 
+
+def test_get_agent_id_returns_none_for_empty_file(mocker):
+    mocker.patch("os.path.exists", return_value=True)
+    mocker.patch("builtins.open", mock_open(read_data=""))
+
+    agent_id = get_agent_id()
+    
+    assert agent_id is None
+    
 
 def test_save_agent_id_writes_id_to_file_correctly(mocker):
     content_to_write = "NEW-AGENT-ID-456"
@@ -226,20 +301,12 @@ def test_agent_iteration_handles_new_agent_registration(mocker):
     fake_osname = "test-os 19"
 
     mock_get_id = mocker.patch("src.agent.core.state.get_agent_id", return_value=None)
-    mock_hostname = mocker.patch(
-        "src.agent.core.socket.gethostname", return_value=fake_hostname
-    )
-    mock_osname_system = mocker.patch(
-        "src.agent.core.platform.system", return_value="test-os"
-    )
-    mock_osname_release = mocker.patch(
-        "src.agent.core.platform.release", return_value="19"
-    )
+    mock_hostname = mocker.patch("src.agent.core.socket.gethostname", return_value=fake_hostname)
+    mock_osname_system = mocker.patch("src.agent.core.platform.system", return_value="test-os")
+    mock_osname_release = mocker.patch("src.agent.core.platform.release", return_value="19")
 
     mock_checkin_response = {"agentId": fake_new_id, "task": "no-task-for-now"}
-    mock_checkin = mocker.patch(
-        "src.agent.core.comms.perform_checkin", return_value=mock_checkin_response
-    )
+    mock_checkin = mocker.patch("src.agent.core.comms.perform_checkin", return_value=mock_checkin_response)
 
     mock_save_id = mocker.patch("src.agent.core.state.save_agent_id")
 
@@ -429,3 +496,38 @@ def test_agent_iteration_handles_checkin_failure_gracefully(mocker):
     mock_print.assert_any_call(
         f"[INFO] Check-in failed. Trying again in {fake_sleep}s."
     )
+
+
+def test_agent_iteration_handles_with_no_agent_in_checkin(mocker):
+    fake_api_url = "https://c2server.example.com/checkin"
+    fake_results_url = "https://c2server.example.com/results"
+    fake_sleep = 60
+    fake_agent_id = "EXISTING-AGENT-ID-456"
+    fake_hostname = "test-host"
+    fake_osname = "test-os 19"
+    fake_task = "test-task"
+
+    mock_get_id = mocker.patch("src.agent.core.state.get_agent_id", return_value=fake_agent_id)
+    mock_hostname = mocker.patch("src.agent.core.socket.gethostname", return_value=fake_hostname)
+    mock_osname_system = mocker.patch("src.agent.core.platform.system", return_value="test-os")
+    mock_osname_release = mocker.patch("src.agent.core.platform.release", return_value="19")
+
+    mock_checkin_response = {"task": fake_task}
+    mock_checkin = mocker.patch("src.agent.core.comms.perform_checkin", return_value=mock_checkin_response)
+
+    expected_task_out = "test-task run successfully"
+    mock_execute_task = mocker.patch("src.agent.core.tasking.execute_task", return_value=expected_task_out)
+    mock_send_results = mocker.patch("src.agent.core.comms.send_results", return_value=True)
+
+    agent_iteration(fake_api_url, fake_results_url, fake_sleep)
+
+    expected_payload = {
+        "hostname": fake_hostname,
+        "os_name": fake_osname,
+        "agentId": fake_agent_id,
+    }
+
+    mock_checkin.assert_called_once_with(fake_api_url, expected_payload)
+
+    mock_execute_task.assert_called_once_with(fake_task)
+    mock_send_results.assert_called_once_with(fake_results_url, fake_agent_id, expected_task_out)
